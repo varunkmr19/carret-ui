82f83218c324b67efb651b086c26c5ed0d009071
varunkmr19 <varun-kumar@live.com>
2021-09-05 11:46:26 +0530

    added navbar and router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Product from './Components/Product';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Product />
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Product} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} /> 
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Login extends Component{
+  constructor(props){
+      super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Login page</h2>
+      </div>
+    )
+  }
+}
+export default Login;
